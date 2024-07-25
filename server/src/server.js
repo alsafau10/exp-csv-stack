d0fe1bbab84db7f2d4e6a4ce941b93a558bf267f
alsafau10 <sayaituburiq3000@gmail.com>
2024-07-25 17:04:10 +0700

    add connection into mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,25 @@
 const http = require('http') ;
-
+const mongoose = require('mongoose');
 const app = require('./app');
+require('dotenv/config');
 
 const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.argv.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', ()=>{
+    console.info(('Mongo connected'));
+})
+
+mongoose.connection.on('error',(error)=>{
+    console.error(error);
+})
+
 async function startServer(){
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
     server.listen(PORT, ()=>{
         console.log(`listening on ${PORT}`);
